ac6556b75bfed56f8932f354afb39d83207d6c76
Yangbin <yangkghjh@gmail.com>
2020-10-30 14:00:24 +0000

    parse single paramater in unraid template

--- cli/modules/unraid/source.go
+++ cli/modules/unraid/source.go
@@ -63,89 +63,147 @@ func InitFunc(p *pipe.Pipe) error {
 	}
 
 	for _, a := range feed.AppList {
-		if !a.Plugin {
-			ctx := pipe.NewContext(a.Name, "")
-
-			data := viper.New()
-			data.Set("description", a.Description)
-			appyml := &app.App{
-				Name: a.Name,
-				Data: data,
-			}
-			ctx.Set("app", appyml)
-
-			iconURL := icon.Icon{
-				URL: a.Icon,
-			}
-			ctx.Set("icon", &iconURL)
-
-			// docker compose
-			env := map[string]*string{}
-			if a.Environment != nil {
-				if v, ok := cast.ToStringMap(a.Environment)["Variable"]; ok {
-					for _, pair := range cast.ToSlice(v) {
-						kv := cast.ToStringMapString(pair)
-						key := kv["Name"]
-						if key != "" {
-							value := kv["Value"]
-							env[key] = &value
-						}
-					}
-				}
-			}
-			ports := []types.ServicePortConfig{}
-			if a.Networking != nil {
-				if publish, ok := cast.ToStringMap(a.Networking)["Publish"]; ok {
-					if pts, ok := cast.ToStringMap(publish)["Port"]; ok {
-						for _, port := range cast.ToSlice(pts) {
-							kv := cast.ToStringMapString(port)
-							if kv["ContainerPort"] != "" {
-								protocol := kv["Protocol"]
-								if protocol != "tcp" && protocol != "udp" {
-									protocol = "tcp"
-								}
-								ports = append(ports, types.ServicePortConfig{
-									Published: cast.ToUint32(kv["HostPort"]),
-									Target:    cast.ToUint32(kv["ContainerPort"]),
-									Protocol:  protocol,
-								})
-							}
-						}
-					}
-				}
-			}
-			volumes := []types.ServiceVolumeConfig{}
-			if a.Networking != nil {
-				if vs, ok := cast.ToStringMap(a.Data)["Volume"]; ok {
-					for _, v := range cast.ToSlice(vs) {
-						kv := cast.ToStringMapString(v)
-						if kv["ContainerDir"] != "" {
-							volumes = append(volumes, types.ServiceVolumeConfig{
-								Target: kv["ContainerDir"],
-								Source: kv["HostDir"],
-							})
-						}
-					}
-				}
-			}
-			dc := &types.Config{
-				Services: []types.ServiceConfig{
-					{
-						ContainerName: a.Name,
-						Environment:   env,
-						Volumes:       volumes,
-						Ports:         ports,
-						Image:         a.Repository,
-					},
-				},
-			}
-			ctx.Set("compose", &compose.Compose{
-				Config: dc,
-			})
-
-			p.Apps = append(p.Apps, ctx)
+		if a.Plugin || a.Repository == "" {
+			continue
 		}
+
+		ctx := pipe.NewContext(a.Name, "")
+
+		ctx.Set("app", a.GetApp())
+		ctx.Set("icon", a.GetIcon())
+
+		// docker compose
+		service := types.ServiceConfig{}
+		service.ContainerName = a.Name
+		service.Environment = a.GetEnvironment()
+		service.Ports = a.GetPorts()
+		service.Volumes = a.GetVolumns()
+		service.Image = a.Repository
+
+		ctx.Set("compose", &compose.Compose{
+			Config: &types.Config{
+				Services: []types.ServiceConfig{service},
+			},
+		})
+
+		p.Apps = append(p.Apps, ctx)
 	}
 
 	return nil
+}
+
+// GetApp parse app metadata from application
+func (a *Application) GetApp() *app.App {
+	data := viper.New()
+	data.Set("description", a.Description)
+	appyml := &app.App{
+		Name: a.Name,
+		Data: data,
+	}
+
+	return appyml
+}
+
+// GetIcon parse icon data from application
+func (a *Application) GetIcon() *icon.Icon {
+	return &icon.Icon{
+		URL: a.Icon,
+	}
+}
+
+// GetEnvironment parse environment of application
+func (a *Application) GetEnvironment() map[string]*string {
+	env := map[string]*string{}
+	if a.Environment == nil {
+		return env
+	}
+
+	v, ok := cast.ToStringMap(a.Environment)["Variable"]
+	if !ok {
+		return env
+	}
+
+	vs := cast.ToSlice(v)
+	if len(vs) == 0 {
+		vs = []interface{}{v}
+	}
+
+	for _, pair := range vs {
+		kv := cast.ToStringMapString(pair)
+		key := kv["Name"]
+		if key != "" {
+			value := kv["Value"]
+			env[key] = &value
+		}
+	}
+
+	return env
+}
+
+// GetPorts parse ports of application
+func (a *Application) GetPorts() []types.ServicePortConfig {
+	ports := []types.ServicePortConfig{}
+	if a.Networking == nil {
+		return ports
+	}
+
+	publish, ok := cast.ToStringMap(a.Networking)["Publish"]
+	if !ok {
+		return ports
+	}
+
+	pts, ok := cast.ToStringMap(publish)["Port"]
+	if !ok {
+		return ports
+	}
+
+	ps := cast.ToSlice(pts)
+	if len(ps) == 0 {
+		ps = []interface{}{pts}
+	}
+
+	for _, port := range ps {
+		kv := cast.ToStringMapString(port)
+		if kv["ContainerPort"] != "" {
+			protocol := kv["Protocol"]
+			if protocol != "tcp" && protocol != "udp" {
+				protocol = "tcp"
+			}
+			ports = append(ports, types.ServicePortConfig{
+				Published: cast.ToUint32(kv["HostPort"]),
+				Target:    cast.ToUint32(kv["ContainerPort"]),
+				Protocol:  protocol,
+			})
+		}
+	}
+
+	return ports
+}
+
+// GetVolumns parse environment of application
+func (a *Application) GetVolumns() []types.ServiceVolumeConfig {
+	volumes := []types.ServiceVolumeConfig{}
+	if a.Networking == nil {
+		return volumes
+	}
+	vs, ok := cast.ToStringMap(a.Data)["Volume"]
+	if !ok {
+		return volumes
+	}
+
+	vos := cast.ToSlice(vs)
+	if len(vos) == 0 {
+		vos = []interface{}{vs}
+	}
+
+	for _, v := range vos {
+		kv := cast.ToStringMapString(v)
+		if kv["ContainerDir"] != "" {
+			volumes = append(volumes, types.ServiceVolumeConfig{
+				Target: kv["ContainerDir"],
+				Source: kv["HostDir"],
+			})
+		}
+	}
+	return volumes
 }
